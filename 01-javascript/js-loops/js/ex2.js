6d9b26b137212a7036659bcdc593e8ac0b377e37
William of Ockham <certainthings@gmail.com>
2017-07-26 12:25:47 +1000

    js loops

---
+++ 01-javascript/js-loops/js/ex2.js
@@ -0,0 +1,18 @@
+/*
+Multiplication Tables
+
+Write a for loop that will iterate from 0 to 10. For each iteration of the for loop, it will multiply the number by 9 and log the result (e.g. "2 * 9 = 18").
+
+Bonus: Use a nested for loop to show the tables for every multiplier from 1 to 10 (100 results total).
+
+*/
+
+for (let i = 0; i <= 10; i++) {
+  console.log( `${ i } * 9 = ${ i * 9 }` );
+}
+
+for (let i = 0; i <= 10; i++) {
+  for (let j = 1; j <= 10; j++ ) {
+    console.log( `${ i } * ${ j } = ${ i * j }` );
+  }
+}
