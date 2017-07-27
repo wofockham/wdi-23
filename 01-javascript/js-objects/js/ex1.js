ee8e4c141e13c386e498d052a26b138b754ecd8c
William of Ockham <certainthings@gmail.com>
2017-07-27 12:32:56 +1000

    javascript object exercises

---
+++ 01-javascript/js-objects/js/ex1.js
@@ -0,0 +1,29 @@
+/*
+The Recipe Card
+
+Never forget another recipe!
+
+Create an object to hold information on your favorite recipe. It should have properties for title (a string), servings (a number), and ingredients (an array of strings).
+
+On separate lines (one console.log statement for each), log the recipe information so it looks like:
+
+Mole
+Serves: 2
+Ingredients:
+cinnamon
+cumin
+cocoa
+*/
+
+let recipe = { // Object literal
+  title: "Butterscotch",
+  servings: 1,
+  ingredients: ['butter', 'scotch']
+};
+
+console.log( recipe.title );
+console.log( `Serves: ${ recipe.servings }`);
+
+for (let i = 0; i < recipe.ingredients.length; i++) {
+  console.log(recipe.ingredients[i]);
+}
---
+++ 01-javascript/js-objects/js/ex2.js
@@ -0,0 +1,26 @@
+/*
+The Reading List
+
+Keep track of which books you read and which books you want to read!
+
+Create an array of objects, where each object describes a book and has properties for the title (a string), author (a string), and alreadyRead (a boolean indicating if you read it yet).
+Iterate through the array of books. For each book, log the book title and book author like so: "The Hobbit by J.R.R. Tolkien".
+Now use an if/else statement to change the output depending on whether you read it yet or not. If you read it, log a string like 'You already read "The Hobbit" by J.R.R. Tolkien', and if not, log a string like 'You still need to read "The Lord of the Rings" by J.R.R. Tolkien.'
+*/
+
+let library = [
+  {title: 'Supergods', author: 'Grant Morrison', alreadyRead: false},
+  {title: 'Ulysses', author: 'James Joyce', alreadyRead: true},
+  {title: 'Captain Underpants', author: 'Davo', alreadyRead: false}
+];
+
+for (let i = 0; i < library.length; i++) {
+  let book = library[i];
+  let info = `${ book.title } by ${ book.author }`;
+
+  if (book.alreadyRead) {
+    console.log(`You already read ${ info }`);
+  } else {
+    console.log(`You still need to read ${ info }`);
+  }
+}
