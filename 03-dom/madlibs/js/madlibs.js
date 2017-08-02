58ebdec0fd2f945ea2a2cf61ac197666056079fa
William of Ockham <certainthings@gmail.com>
2017-08-02 14:58:25 +1000

    madlibs input example

---
+++ 03-dom/madlibs/js/madlibs.js
@@ -0,0 +1,12 @@
+const button = document.getElementById('lib-button');
+
+const makeStory = function () {
+  const noun = document.getElementById('noun').value;
+  const adjective = document.getElementById('adjective').value;
+  const person = document.getElementById('person').value;
+
+  const story = `${person} really likes ${adjective} ${noun}`;
+  document.getElementById('story').innerHTML = story;
+}
+
+button.addEventListener('click', makeStory);
