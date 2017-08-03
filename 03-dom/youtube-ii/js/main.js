c320b823352cbb52759ff4c1d8e8d13acf41568e
William of Ockham <certainthings@gmail.com>
2017-08-03 14:48:36 +1000

    const over let

--- 03-dom/youtube-ii/js/main.js
+++ 03-dom/youtube-ii/js/main.js
@@ -1,6 +1,6 @@
 $('a').each(function (index, element) {
   const href = $(element).attr('href');
   const thumbnailURL = youtube.generateThumbnailUrl( href );
-  let $img = $('<img>').attr('src', thumbnailURL);
+  const $img = $('<img>').attr('src', thumbnailURL);
   $(element).append($img);
 });
