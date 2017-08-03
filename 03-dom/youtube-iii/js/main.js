637aad1a216e9ad954f0e37191a1512b71ddb1e4
William of Ockham <certainthings@gmail.com>
2017-08-03 21:05:14 +1000

    youtube with jquery player

---
+++ 03-dom/youtube-iii/js/main.js
@@ -0,0 +1,17 @@
+const thumbnailify = function (index, element) {
+  const href = $(element).attr('href');
+  const thumbnailURL = youtube.generateThumbnailUrl( href );
+  const $img = $('<img>').attr('src', thumbnailURL);
+  $(element).append($img);
+
+  $(element).on('click', function (event) {
+    event.preventDefault(); // Disable the link
+
+    const embedURL = youtube.generateEmbedUrl( href );
+    const embedHTML = `<iframe width="560" height="315" src="${embedURL}" frameborder="0" allowfullscreen></iframe>`;
+
+    $('#player').hide().html( embedHTML ).fadeIn(1800); // Chaining
+  });
+}
+
+$('a').each( thumbnailify );
