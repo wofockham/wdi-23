8555abd4d88e60ba9ab60e66ca9bcdcdeb63ae3d
William of Ockham <certainthings@gmail.com>
2017-08-16 11:44:17 +1000

    sinatra intro

--- 06-sinatra/sinatra-intro/main.rb
+++ 06-sinatra/sinatra-intro/main.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'sinatra/reloader'
 
 get '/' do
   "This is the homepage"
@@ -6,4 +7,25 @@ end
 
 get '/hello' do
   "Hello world from Sinatra"
+end
+
+get '/family/:name' do
+  "Hello #{ params[:name] }"
+end
+
+get '/name/:first' do
+  "Your name is #{ params[:first].capitalize }"
+end
+
+get '/name/:first/:last' do
+  "Your name is #{ params[:first].capitalize } #{ params[:last].upcase }"
+end
+
+get '/name/:first/:last/:age' do
+  "Your name is #{ params[:first].capitalize } #{ params[:last].upcase } and your age is #{ params[:age] }"
+end
+
+get '/multiply/:x/:y' do
+  result = params[:x].to_f * params[:y].to_f
+  "The result is #{ result }"
 end
