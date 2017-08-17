7ecd8ca955ca6f9f57ddea81f1460dcc2237caf3
William of Ockham <certainthings@gmail.com>
2017-08-17 15:14:24 +1000

    complete butterfly CRUD system

--- 07-databases/butterflies/main.rb
+++ 07-databases/butterflies/main.rb
@@ -8,25 +8,54 @@ end
 
 # INDEX
 get '/butterflies' do
-  db = SQLite3::Database.new 'database.sqlite3'
-  db.results_as_hash = true
-
-  @butterflies = db.execute "SELECT * FROM butterflies"
-
-  db.close
-
+  @butterflies = query_db "SELECT * FROM butterflies"
   erb :butterflies_index
+end
+
+# NEW
+get '/butterflies/new' do
+  erb :butterflies_new
+end
+
+# CREATE
+post '/butterflies' do
+  query_db "INSERT INTO butterflies (name, family, image) VALUES ('#{ params["name"] }', '#{ params["family"] }', '#{ params["image"] }')"
+  redirect to('/butterflies')
 end
 
 # SHOW
 get '/butterflies/:id' do
+  @butterfly = query_db "SELECT * FROM butterflies WHERE id=#{ params['id'] }"
+  @butterfly = @butterfly.first # Strip off the container array
+  erb :butterflies_show
+end
+
+# EDIT
+get '/butterflies/:id/edit' do
+  @butterfly = query_db "SELECT * FROM butterflies WHERE id=#{ params['id'] }"
+  @butterfly = @butterfly.first
+  erb :butterflies_edit
+end
+
+# UPDATE
+post '/butterflies/:id' do
+  query_db "UPDATE butterflies SET name='#{ params['name'] }', family='#{ params['family'] }', image='#{ params['image'] }' WHERE id=#{ params['id'] }"
+  redirect to("/butterflies/#{ params['id'] }")
+end
+
+# DESTROY
+get '/butterflies/:id/delete' do
+  query_db "DELETE FROM butterflies WHERE id=#{ params['id'] }"
+  redirect to('/butterflies')
+end
+
+def query_db(sql)
   db = SQLite3::Database.new 'database.sqlite3'
   db.results_as_hash = true
 
-  @butterfly = db.execute "SELECT * FROM butterflies WHERE id=#{ params[:id] }"
-  @butterfly = @butterfly.first # Strip off the container array
+  puts sql # Optional but nice for debugging.
 
+  results = db.execute sql
   db.close
-
-  erb :butterflies_show
+  results # Implicit return
 end
