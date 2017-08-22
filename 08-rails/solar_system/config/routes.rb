98c464940a46ad0f05d059197f408e5859447cae
William of Ockham <certainthings@gmail.com>
2017-08-22 14:48:13 +1000

    working planet crud system

--- 08-rails/solar_system/config/routes.rb
+++ 08-rails/solar_system/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get '/planets' => 'planets#index'
-  get '/planets/new' => 'planets#new'
+  get '/planets/new' => 'planets#new', :as => 'planet_new'
   post '/planets' => 'planets#create'
+  get '/planets/:id' => 'planets#show', :as => 'planet'
+  post '/planets/:id/delete' => 'planets#destroy', :as => 'planet_delete'
+  get '/planets/:id/edit' => 'planets#edit', :as => 'planet_edit'
+  post '/planets/:id' => 'planets#update'
 end
