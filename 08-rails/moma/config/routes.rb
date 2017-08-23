de3ceb9e75befbd4b0b28bc67aa27f6aea5f4605
William of Ockham <certainthings@gmail.com>
2017-08-23 15:04:02 +1000

    works crud

--- 08-rails/moma/config/routes.rb
+++ 08-rails/moma/config/routes.rb
@@ -9,8 +9,17 @@
 #             PATCH  /artists/:id(.:format)      artists#update
 #             PUT    /artists/:id(.:format)      artists#update
 #             DELETE /artists/:id(.:format)      artists#destroy
+#       works GET    /works(.:format)            works#index
+#             POST   /works(.:format)            works#create
+#    new_work GET    /works/new(.:format)        works#new
+#   edit_work GET    /works/:id/edit(.:format)   works#edit
+#        work GET    /works/:id(.:format)        works#show
+#             PATCH  /works/:id(.:format)        works#update
+#             PUT    /works/:id(.:format)        works#update
+#             DELETE /works/:id(.:format)        works#destroy
 #
 
 Rails.application.routes.draw do
   resources :artists
+  resources :works
 end
