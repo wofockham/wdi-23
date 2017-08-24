7e2aa5413eaa29cccaa08dbbc6ba96406f080ee1
William of Ockham <certainthings@gmail.com>
2017-08-24 12:28:33 +1000

    tunr seed data

--- 08-rails/tunr/db/seeds.rb
+++ 08-rails/tunr/db/seeds.rb
@@ -1,7 +1,50 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+u1 = User.create :email => 'craigsy@ga.co'
+u2 = User.create :email => 'jonesy@ga.co'
+
+Song.destroy_all
+s1 = Song.create :name => 'Have A Safe Trip Dear'
+s2 = Song.create :name => 'Toxic'
+s3 = Song.create :name => 'Carpet Yankers'
+
+Album.destroy_all
+a1 = Album.create :name => 'Engine Takes To The Water'
+a2 = Album.create :name => 'In The Zone'
+a3 = Album.create :name => 'Trance Planet Sugar Mouth'
+
+Artist.destroy_all
+r1 = Artist.create :name => 'June of 44'
+r2 = Artist.create :name => 'Britney Spears'
+r3 = Artist.create :name => 'Shrapnel'
+
+Genre.destroy_all
+g1 = Genre.create :name => 'Math rock'
+g2 = Genre.create :name => 'Chemical Bollywood'
+g3 = Genre.create :name => 'Dad rock'
+
+Mixtape.destroy_all
+m1 = Mixtape.create :name => 'Make Out Music'
+m2 = Mixtape.create :name => 'Code Jams'
+m3 = Mixtape.create :name => 'House cleaning music'
+
+################################################################################
+# Associations
+
+a1.songs << s1
+a2.songs << s2
+a3.songs << s3
+
+r1.songs << s1
+r2.songs << s2
+r3.songs << s3
+
+s1.genres << g1
+s2.genres << g2 << g3
+s3.genres << g3
+
+m1.songs << s2 << s3
+m2.songs << s2 << s1 << s2 << s3 << s2
+m3.songs = [s1, s2, s3]
+
+u1.mixtapes << m1 << m2
+u2.mixtapes << m3
