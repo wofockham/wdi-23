65438764df046e2fa8827738093ff2fb6a50ceb1
William of Ockham <certainthings@gmail.com>
2017-08-25 12:08:41 +1000

    sign in seems to work

--- 08-rails/tunr/app/controllers/session_controller.rb
+++ 08-rails/tunr/app/controllers/session_controller.rb
@@ -5,9 +5,15 @@ class SessionController < ApplicationController
   def create
     user = User.find_by :email => params[:email]
     if user.present? && user.authenticate(params[:password])
+      session[:user_id] = user.id
       redirect_to root_path
     else
       redirect_to login_path
     end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
   end
 end
