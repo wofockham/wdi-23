ed55bf757ba5e57ed369c886e5fbc94138045ab7
William of Ockham <certainthings@gmail.com>
2017-09-05 15:17:14 +1000

    jquery ajax

--- 10-ajax/books/js/books.js
+++ 10-ajax/books/js/books.js
@@ -1,27 +1,16 @@
 const fetchBook = function () {
   const title = document.getElementById('title').value;
-  // create our XHR
-  const xhr = new XMLHttpRequest();
-  // set up the XHR
-  xhr.open('GET', `https://www.googleapis.com/books/v1/volumes?q=title:${title}`);
 
-  // attach our onreadystatechange
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState !== 4) { return; }
-
-    const info = JSON.parse( xhr.responseText );
-
-    // find the cover
+  $.ajax(`https://www.googleapis.com/books/v1/volumes?q=title:${title}`).done(function (info) {
     const cover = info.items[0].volumeInfo.imageLinks.thumbnail;
-
-    // display the cover
-    const img = document.createElement('img');
-    img.src = cover;
-    document.querySelector('main').appendChild( img );
-  }
-
-  xhr.send();
+    $('<img>').attr('src', cover).appendTo('main');
+  }).done(function (i) {
+    console.log(i);
+  }).fail(function () {
+    alert('There was an error');
+  });
 };
 
-const button = document.getElementById('search');
-button.addEventListener('click', fetchBook);
+$(document).ready(function () {
+  $('#search').on('click', fetchBook);
+});
