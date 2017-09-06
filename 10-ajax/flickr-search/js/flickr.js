f2cc0161ade68cc7663e9c735f5efd467b406130
William of Ockham <certainthings@gmail.com>
2017-09-06 15:05:54 +1000

    flickr search with the skellington of infinite scroll

--- 10-ajax/flickr-search/js/flickr.js
+++ 10-ajax/flickr-search/js/flickr.js
@@ -1,3 +1,27 @@
+const showImages = function (results) {
+  const generateURL = function (photo) {
+    return [
+      'http://farm',
+      photo.farm,
+      '.static.flickr.com/',
+      photo.server,
+      '/',
+      photo.id,
+      '_',
+      photo.secret,
+      '_q.jpg' // Change the q for different sizes
+    ].join('');
+  };
+
+  // console.log( results );
+
+  _(results.photos.photo).each(function (p) {
+    const imageURL = generateURL(p);
+    const $img = $('<img>', {src: imageURL}); // Equivalent .attr('src', imageURL);
+    $img.appendTo('#images');
+  });
+};
+
 const searchFlickr = function (q) {
   console.log('Searching Flickr for', q);
 
@@ -8,17 +32,28 @@ const searchFlickr = function (q) {
     api_key: '2f5ac274ecfac5a455f38745704ad084',
     text: q,
     format: 'json'
-  }).done(function (results) {
-    console.log( results );
-  })
-}
+  }).done(showImages);
+};
 
 $(document).ready(function () {
   $('#search').on('submit', function (e) {
     e.preventDefault();
 
     const query = $('#query').val();
-
     searchFlickr( query );
-  })
+  });
+
+  $(window).on('scroll', function () {
+    const documentHeight = $(document).height();
+    const windowHeight = $(window).height();
+    const scrollTop = $(window).scrollTop();
+
+    const scrollBottom = documentHeight - (scrollTop + windowHeight);
+
+    if (scrollBottom < 500) {
+      const query = $('#query').val();
+      searchFlickr( query );
+    }
+
+  });
 });
