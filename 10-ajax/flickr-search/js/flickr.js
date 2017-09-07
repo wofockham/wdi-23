1bacd4a3a692b61a981f7a850670d6efdd3fad85
William of Ockham <certainthings@gmail.com>
2017-09-07 10:49:05 +1000

    prevent requests after the last page is reached

--- 10-ajax/flickr-search/js/flickr.js
+++ 10-ajax/flickr-search/js/flickr.js
@@ -1,33 +1,48 @@
 const state = {
-  currentPage: 1
+  currentPage: 1,
+  lastPage: false
 };
 
 const showImages = function (results) {
     const generateURL = function (photo) {
-    return [
-      'http://farm',
-      photo.farm,
-      '.static.flickr.com/',
-      photo.server,
-      '/',
-      photo.id,
-      '_',
-      photo.secret,
-      '_q.jpg' // Change the q for different sizes
-    ].join('');
+      return [
+        'http://farm',
+        photo.farm,
+        '.static.flickr.com/',
+        photo.server,
+        '/',
+        photo.id,
+        '_',
+        photo.secret,
+        '_q.jpg' // Change the q for different sizes
+      ].join('');
   };
 
-  // console.log( results );
+  if (results.photos.page === results.photos.pages) {
+    state.lastPage = true;
+  }
 
   _(results.photos.photo).each(function (p) {
     const imageURL = generateURL(p);
     const $img = $('<img>', {src: imageURL}); // Equivalent .attr('src', imageURL);
-    $img.appendTo('#images');
+    // Use the full size image URL rather than a link to flickr.com
+    const $a = $('<a></a>', {href: 'http://flickr.com/', target: '_blank'});
+    $a.html($img);
+
+    $a.on('click', function () {
+      alert( p.farm );
+    })
+
+    $a.appendTo('#images');
   });
 
 };
 
 const searchFlickr = function (q) {
+
+  if (state.lastPage) {
+    return;
+  }
 
   console.log('Searching Flickr for', q);
 
@@ -49,6 +64,7 @@ $(document).ready(function () {
 
     $('#images').empty();
     state.currentPage = 1;
+    state.lastPage = false;
 
     const query = $('#query').val();
     searchFlickr( query );
