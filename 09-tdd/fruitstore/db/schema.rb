f8f453c4f9a0cd95c28fb173e5797fd1b003c094
William of Ockham <certainthings@gmail.com>
2017-09-28 12:08:16 +1000

    single table inheritance example

--- 09-tdd/fruitstore/db/schema.rb
+++ 09-tdd/fruitstore/db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170928004728) do
+ActiveRecord::Schema.define(version: 20170928015450) do
 
   create_table "fruits", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "type"
   end
 
 end
--- 09-tdd/fruitstore/spec/models/fruit_spec.rb
+++ 09-tdd/fruitstore/spec/models/fruit_spec.rb
@@ -2,21 +2,42 @@ require 'rails_helper'
 
 RSpec.describe Fruit, type: :model do
   describe "An apple" do
-    it "should not be squishy" do
-      @apple = Fruit.new
+    before do
+      @apple = Apple.create :name => 'Granny Smith'
+    end
 
+    it "should not be squishy" do
       expect(@apple.squishy?).to eq false
       expect(@apple.squishy?).to be false
     end
+
+    it "should remember the class using Single Table Inheritance (STI)" do
+      apple = Fruit.find @apple.id
+      expect(apple).to_not be_nil
+      expect(apple.class).to eq Apple
+      expect(apple).to eq @apple
+      expect(apple.is_a?(Fruit)).to be true
+      expect(apple.class.ancestors).to include(Fruit)
+    end
   end
 
-
   describe "A pear" do
-    it "should be squishy" do
-      @pear = Fruit.new
+    before do
+      @pear = Pear.create :name => 'Bosc'
+    end
 
+    it "should be squishy" do
       expect(@pear.squishy?).to eq true
       expect(@pear.squishy?).to be true
+    end
+
+    it "should remember the class using Single Table Inheritance (STI)" do
+      pear = Fruit.find @pear.id
+      expect(pear).to_not be_nil
+      expect(pear.class).to eq Pear
+      expect(pear).to eq @pear
+      expect(pear.is_a?(Fruit)).to be true
+      expect(pear.class.ancestors).to include(Fruit)
     end
   end
 end
