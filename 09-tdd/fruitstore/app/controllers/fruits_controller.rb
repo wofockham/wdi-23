63a75c3f19eec6a3e164e7c0181e983902ad6b26
William of Ockham <certainthings@gmail.com>
2017-09-28 15:42:37 +1000

    post fruit with valid and invalid information

--- 09-tdd/fruitstore/app/controllers/fruits_controller.rb
+++ 09-tdd/fruitstore/app/controllers/fruits_controller.rb
@@ -1,5 +1,10 @@
 class FruitsController < ApplicationController
   def index
     @fruits = Fruit.order('created_at DESC')
+
+    respond_to do |format|
+      format.html {} # An empty block here means render the default view
+      format.json { render :json => @fruits }
+    end
   end
 end
