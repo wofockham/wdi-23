7071253bbb960daed4379dcd95211ad4b6a774bb
William of Ockham <certainthings@gmail.com>
2017-09-29 16:31:07 +1000

    stupid front end tricks part 2

--- 14-advanced/stupid/js/stupid.js
+++ 14-advanced/stupid/js/stupid.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+  // Parallax effects //////////////////////////////////////////////////////////
   const $body = $('body');
   const $bill = $('.bill');
 
@@ -6,5 +7,26 @@ $(document).ready(function () {
     const scrollTop = $(window).scrollTop();
     $body.css('background-position-y', -scrollTop / 3);
     $bill.css('background-position-y', -scrollTop * 0.75);
+  });
+
+  // Bubbles ///////////////////////////////////////////////////////////////////
+  $(window).on('mousemove', function (e) {
+    const x = e.pageX;
+    const y = e.pageY;
+
+    const size = (Math.random() * 10) + 'em';
+
+    const $bubble = $('<div class="bubble"></div>').css({
+      left: x,
+      top: y,
+      width: size,
+      height: size
+    });
+
+    $bubble.appendTo($body);
+
+    $bubble.animate({top: -200}, 2000, function () {
+      $bubble.remove(); // Clean up to conserve memory.
+    });
   });
 });
