b92abc8301072212190d56f101738d8f6d542ca6
William of Ockham <certainthings@gmail.com>
2017-09-29 11:04:10 +1000

    xmas steps

---
+++ 09-tdd/calculator/calculator.rb
@@ -0,0 +1,15 @@
+class Calculator
+  def initialize
+    @input = []
+  end
+
+  # An array style append
+  def <<(n)
+    @input.push n.to_f
+  end
+
+  def add
+    # @input.inject { |memo, n| memo + n }
+    @input.inject(&:+)
+  end
+end
---
+++ 09-tdd/calculator/features/step_definitions/calculator_steps.rb
@@ -0,0 +1,15 @@
+Given("I have a calculator") do
+  @calculator = Calculator.new
+end
+
+Given("I have the number {int}") do |int|
+  @calculator << int
+end
+
+When("I add them together") do
+  @result = @calculator.add
+end
+
+Then("I should see {int} on the terminal") do |int|
+  assert_equal int.to_f, @result
+end
