7ee5da946b10dbbf3b5e4bd882d5c4d1b14e52f7
William of Ockham <certainthings@gmail.com>
2017-10-04 11:03:20 +1100

    call/apply/bind/...

---
+++ 14-advanced/this/this-demo.js
@@ -0,0 +1,30 @@
+const groucho = {
+  name: 'Groucho Julius Marx',
+  greeting: function () {
+    return 'Good evening, my name is ' + this.name;
+  }
+};
+
+const harpo = {
+  name: 'Harpo Marx',
+  greeting: function () {
+    return 'Good evening, my name is ' + this.name;
+  }
+};
+
+// .bind ///////////////////////////////////////////////////////////////////////
+// This lets you permanantly associate a particular object as the `this` within
+// the function, REGARDLESS of where/how you call the function.
+
+const grouchoGreeter = groucho.greeting.bind(groucho);
+grouchoGreeter(); // `this` in the fn will refer to `groucho`
+
+// .apply //////////////////////////////////////////////////////////////////////
+// This lets us pass an array argument into a function which expects multiple
+// separate parameters.
+const numbers = [134, 34, 3, 4, -1001, 0, 12, 100000];
+// Math.max( numbers ); // Doesn't work because .max expects separate parameters.
+// ES5:
+Math.max.apply(null, numbers); // Flattening out the numbers array into separate params.
+// ES6:
+Math.max( ...numbers ); // Destructuring or splatting.
