2b9f50206ecb47913eafb97209509bb06b47f1c2
William of Ockham <certainthings@gmail.com>
2017-10-05 10:56:36 +1100

    DIY preact skellington

---
+++ 14-advanced/github-search/src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div class="app">
+        <h1>Hello</h1>
+      </div>
+    );
+  }
+}
---
+++ 14-advanced/github-search/src/index.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+import 'preact/devtools';
+
+import App from './components/App';
+
+ReactDOM.render(<App />, document.body);
